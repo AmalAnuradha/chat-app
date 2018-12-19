ec6a419010a7d3b8636dd76d6c56864a08f3c2d2
Charith Bandara <charith@persystance.com>
2018-12-19 23:20:13 +0530

    latest

--- src/components/Dashboard/Views/GroupMessages.vue
+++ src/components/Dashboard/Views/GroupMessages.vue
@@ -15,30 +15,46 @@
           <div class="col-xs-12 form-group">
           <div> 
               <sui-message 
-                style="float: left;width:55%;background-color: beige;"
-                v-if="message.userID._id !== user._id"
-                icon="inbox"
-                v-bind:header="message.userID.firstname"
-                v-bind:content="message.message"
-                />
-                <sui-message 
-                style="float: right;width:55%;background-color: beige;"
-                v-if="message.userID._id == user._id"
-                icon="inbox"
-                v-bind:header="message.userID.firstname"
-                v-bind:content="message.message"
-                />
+                    style="float: left;width:55%;background-color: beige;"
+                    v-if="message.userID._id !== user._id"
+                    icon="inbox">
+                    <sui-message-content>
+                        <sui-message-header>{{message.userID.firstname}}</sui-message-header>
+                        <div v-if="messageType(message.message) === 0">{{message.message}}</div>
+                        <sui-image v-if="messageType(message.message) === 1" v-bind:src="message.message" medium size="medium"/>
+                        <video v-if="messageType(message.message) === 2" width="320" height="240" controls>
+                            <source v-bind:src="message.message" type="video/mp4">
+                        </video>
+                    </sui-message-content>
+                </sui-message>
+
+                <sui-message success 
+                    style="float: right;width:55%;background-color: beige;"
+                    v-if="message.userID._id == user._id"
+                    icon="inbox"
+                >
+                    <sui-message-content>
+                        <sui-message-header>{{message.userID.firstname}}</sui-message-header>
+                        <div v-if="messageType(message.message) === 0">{{message.message}}</div>
+                        <sui-image v-if="messageType(message.message) === 1" v-bind:src="message.message" medium size="medium"/>
+                        <video v-if="messageType(message.message) === 2" width="320" height="240" controls>
+                            <source v-bind:src="message.message" type="video/mp4">
+                        </video>
+                    </sui-message-content>
+                </sui-message>
         </div> 
-          </div>
+        
+        </div>
       </div>
         
       
     </div>
-    <div class="row">
+    <div class="row" v-if="joined">
       <div class="col-md-12 s-m-contianer">
         <div class="ui action left icon input">
           <i class="mail icon"></i>
           <input type="text" placeholder="Message..." v-model="message">
+            <iframe v-if="joined" ref="myIframe" v-bind:src="link" id="myIframe" height="44"></iframe>
           <div class="ui teal button" v-on:click="sendRoomMessage()">Send</div>
         </div>
       </div>
@@ -46,14 +62,16 @@
   </div>
 </template>
 <script>
+
 export default {
   data () {
     return {
       user: '',
       joined: false,
       message: '',
-      roomName: 'public_chat',
-      messages: []
+      roomName: '',
+      messages: [],
+      link: 'http://192.168.0.66:3000/fileuploader.html?'
     }
   },
   sockets: {
@@ -70,11 +88,21 @@ export default {
     let user = JSON.parse(localStorage.getItem('user'))
     if (user) {
       this.user = user
+      this.link = this.link + user._id + ':' + this.roomName
     }
   },
   methods: {
+    messageType: function (text) {
+      if (text.includes('.png') || text.includes('.jpg') || text.includes('.jpeg')) {
+        return 1
+      } else if (text.includes('.mp4') || text.includes('.webm') || text.includes('.mp3')) {
+        return 2
+      }
+      return 0
+    },
     joinRoom: function () {
       this.$socket.emit('join_chat_room', { room: this.roomName })
+      this.link = 'http://192.168.0.66:3000/fileuploader.html?' + this.user._id + ':' + this.roomName
       this.joined = true
     },
     sendRoomMessage: function () {
